6beda28f34eb21bcc00cb7ef9d12a76fda6932e0
Vladimir Mikhaylovskiy <vlad@indiegogo.com>
2017-07-25 10:54:00 -0700

    refactored api to be used as provider

--- frontend/src/js/bootstrap.jsx
+++ frontend/src/js/bootstrap.jsx
@@ -3,37 +3,25 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import ScrollToTop from './components/common/scroll-to-top';
+import ApiProvider from './providers/api-provider';
 import App from './components/app';
-import ApiService from './services/api-service';
-
-import { ApiPropTypes } from './definitions/services-prop-types';
 
 class Bootstrap extends Component {
-  constructor(props) {
-    super(props);
-
-    this.api = new ApiService();
-  }
-
-  getChildContext() {
-    return { api: this.api };
-  }
-
   componentDidMount() {
     // Place on app load script or global fetch here
   }
 
   render() {
     return (
-      <BrowserRouter>
-        <ScrollToTop>
-          <App />
-        </ScrollToTop>
-      </BrowserRouter>
+      <ApiProvider>
+        <BrowserRouter>
+          <ScrollToTop>
+            <App />
+          </ScrollToTop>
+        </BrowserRouter>
+      </ApiProvider>
     );
   }
 }
-
-Bootstrap.childContextTypes = { api: ApiPropTypes };
 
 ReactDOM.render(<Bootstrap />, document.getElementById('container'));
---
+++ frontend/src/js/providers/api-provider.jsx
@@ -0,0 +1,47 @@
+import React, { Component, PropTypes, Children } from 'react';
+
+import { ApiClient, DefaultApi } from '../client/src/index';
+// client will be generated with Swagger
+import { ApiPropTypes } from '../definitions/services-prop-types';
+
+class ApiProvider extends Component {
+  static childContextTypes = { api: ApiPropTypes };
+  static propTypes = { children: PropTypes.element.isRequired };
+
+  constructor(props) {
+    super(props);
+
+    const dev = process.env.NODE_ENV !== 'production';
+    let base = '';
+    if (dev) {
+      base = process.env.BACKEND_HOST
+        ? process.env.BACKEND_HOST
+        : 'http://localhost:8080';
+    }
+
+    const devClient = new ApiClient();
+    devClient.basePath = `${base}/api`;
+
+    this.api = new DefaultApi(devClient);
+  }
+
+  getChildContext() {
+    console.log('initializing api service!');
+    return { api: this.api };
+  }
+
+  render() {
+    return Children.only(this.props.children);
+  }
+}
+
+export default ApiProvider;
+
+export const api = (ComponentToWrap) => {
+  const apiComponent = () =>
+    <ComponentToWrap {...this.props} api={this.context.api} />;
+
+  apiComponent.contextTypes = { api: ApiPropTypes.isRequired };
+
+  return apiComponent;
+};
